12708988e8ccae27f83ba76be0802b113e627d9c
albin-joseph <albinjoseph007@gmail.com>
2025-03-27 17:42:10 +0530

    Updates

--- src/validators/personal/index.ts
+++ src/validators/personal/index.ts
@@ -1,8 +1,11 @@
+import {AgeValidatorOptions, AgeValidationResult, AgeValidator} from './age.validator'
 import {GenderValidator, GenderOption, GenderValidatorOptions, GenderValidationResult} from './gender.validator'
-
 export { NameValidator } from './name.validator'
 
 export {
+    AgeValidatorOptions,
+    AgeValidationResult,
+    AgeValidator,
     GenderValidator,
     GenderOption,
     GenderValidationResult,
